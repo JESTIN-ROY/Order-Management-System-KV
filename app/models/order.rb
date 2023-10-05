1197912e606efb8f3311d653531f3db21c5c7cd9
JESTIN ROY <jestin@keyvalue.systems>
2023-10-05 14:07:22 +0530

    Feat: Added the use cases of model scopes in Order Model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Order < ApplicationRecord
+  scope :pending, -> { where(status: "pending") }
+  scope :completed, -> { where(status: "completed") }
+
   belongs_to :customer
   has_many :order_items, dependent: :destroy
   has_many :products, through: :order_items
